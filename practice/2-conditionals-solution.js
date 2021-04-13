f6514ebb11bb5b80a0ed53647ab409f0f8b9a0d1
Brian Eng <brian@brianeng.com>
2021-04-13 11:45:31 -0500

    Update 2-conditionals-solution.js

--- practice/2-conditionals-solution.js
+++ practice/2-conditionals-solution.js
@@ -4,31 +4,34 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max)) + 1
 }
 
-function pageLoad() {
-  // we start with the dice from the first practice exercise
-  let die1 = getRandomInt(6)
-  let die2 = getRandomInt(6)
-  let total = die1 + die2
-  let output
+// Goal: Play a simple game of craps
+// - Begin with the recipe (algorithm) and code from the first practice
+// - Implement the (basic) rules of craps:
+//   - If the total of the two dice is 7 or 11, the player wins
+//   - If the total of the two dice is 2, 3, or 12, the player loses
+//   - If the total is anything else, the "point" is set, e.g. "the point is 6"
 
-  // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
-  
-  // 🔥 YOUR CODE GOES HERE. 🔥
-  // the value of the variable 'output' is already set to display on the page
-  // write an if.. else if.. else statement to evaluate the value of 'total'
-  // and set the value of 'output' appropriately
-  if (total == 7 || total == 11) {
-    output = 'you win!'
-  } else if (total == 2 || total == 3 || total == 12) {
-    output = 'you lose!'
-  } else {
-    output = `the point is ${total}`
-  }
-  //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
+// Write the recipe (algorithm) in the comments. Write the code.
 
-  // Leave this alone
-  document.querySelector('.die1').setAttribute('src', `../images/dice/${die1}.png`)
-  document.querySelector('.die2').setAttribute('src', `../images/dice/${die2}.png`)
-  document.querySelector('.total').innerHTML = total
-  document.querySelector('.output').innerHTML = output
+// Store the value of the first die in memory. Write it out to the console in a human-readable format.
+let die1 = getRandomInt(6)
+console.log(`The first die is ${die1}`)
+
+// Store the value of the second die in memory. Write it out to the console in a human-readable format.
+let die2 = getRandomInt(6)
+console.log(`The second die is ${die2}`)
+
+// Store the value of the total of the two dice in memory. Write it out to the console in a human-readable format.
+let total = die1 + die2
+console.log(`The total is ${total}`)
+
+if (total == 7 || total == 11) {
+  // If the total of the two dice is 7 or 11, the player wins
+  console.log(`YOU WIN!`)
+} else if (total == 2 || total == 3 || total == 12) {
+  // If the total of the two dice is 2, 3, or 12, the player loses
+  console.log(`YOU LOSE!`)
+} else {
+  // If the total is anything else, the "point" is set, e.g. "the point is 6"
+  console.log(`The point is ${total}`)
 }
