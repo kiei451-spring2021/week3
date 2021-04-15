2c45c0e53aa88ca9a7ac418e80333a92d840e3aa
Benjamin Block <bjblock@gmail.com>
2021-04-15 17:04:28 -0500

    variable declaration

--- code-along-complete/1-types.js
+++ code-along-complete/1-types.js
@@ -1,10 +1,13 @@
 // Goal: A program that stores my lunch order in the computer's memory, then writes it to the console
 // Write the algorithm, in the comments. Write the code.
 
-// Store the item I want to eat
+// Reserve a place in the computer's memory for the thing I want to eat
+let food
+
+// Assign the item I want to eat
 let food = `tacos`
 
-// Store the quantity I want
+// Reserve a place for and store the quantity I want
 let quantity = 3
 
 // Make an English sentence
